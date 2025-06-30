e61fd4948a7bfe97b7dd44434646270b5bcedaeb
maimuna-t-333 <maimuna.tabr@gmail.com>
2025-06-30 12:48:48 +0600

    favicon and logo

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { motion } from 'framer-motion';
+import img from '../../src/assets/Maimunah Tabassum.png'
 
 const navLinks = [
     { name: 'About Me', path: 'about' },
@@ -26,27 +27,36 @@ const itemVariants = {
 
 const Home = () => {
     return (
-        <div className="bg-black w-full h-16 flex items-center justify-end pr-16">
-            <motion.div
-                className="flex gap-6 text-[18px]"
-                variants={containerVariants}
-                initial="hidden"
-                animate="show"
-            >
-                {navLinks.map((link) => (
-                    <motion.div key={link.path} variants={itemVariants}>
-                        <Link
-                            to={link.path}
-                            className="relative group overflow-hidden"
-                        >
-                            <span className="text-white transition duration-300 group-hover:text-white group-hover:drop-shadow-md">
-                                {link.name}
-                            </span>
-                           
-                        </Link>
+        <div className=" bg-black w-full h-16 flex items-center justify-end pr-16">
+            <div className='flex justify-between gap-210 '>
+                <div>
+                    <img className='h-16 w-16' src={img} alt="" />
+                </div>
+                <div>
+                    <motion.div
+                        className="flex gap-6 text-[18px] mt-4"
+                        variants={containerVariants}
+                        initial="hidden"
+                        animate="show"
+                    >
+                        {navLinks.map((link) => (
+                            <motion.div key={link.path} variants={itemVariants}>
+                                <Link
+                                    to={link.path}
+                                    className="relative group overflow-hidden"
+                                >
+                                    <span className="text-white transition duration-300 group-hover:text-white group-hover:drop-shadow-md">
+                                        {link.name}
+                                    </span>
+
+                                </Link>
+                            </motion.div>
+                        ))}
                     </motion.div>
-                ))}
-            </motion.div>
+                </div>
+            </div>
+
+
         </div>
     );
 };
